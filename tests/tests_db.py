5d40f7999a599953f2db4195018499cf2e16dee1
Böhm, Florian <florian.boehm@all-for-one.com>
2022-10-29 20:09:31 +0200

    ADD: setUp and tearDown for unit tests

--- tests/tests_db.py
+++ tests/tests_db.py
@@ -1,10 +1,17 @@
 import unittest
-#import webapp.app
+import webapp.db
 import sqlite3
 
 # run tests: python -m unittest discover tests
 
 class Testclass(unittest.TestCase):
+    def setUp(self):
+        print("setUp")
+        webapp.db.init_db()
+
+    def tearDown(self):
+        print("tearDown")
+        webapp.db.init_db()
 
     def test_get_all(self):
         db = sqlite3.connect('employees.db')
--- webapp/db.py
+++ webapp/db.py
@@ -1,20 +1,24 @@
 import sqlite3
 
-employees = [
-["Peter","Maier",100000,"Development"],
-["Juergen","Dieter",25000,"IT"],
-["Max","Mustermann",30000,"Marketing"]
-]
+def init_db():
+    employees = [
+    ["Peter","Maier",100000,"Development"],
+    ["Juergen","Dieter",25000,"IT"],
+    ["Max","Mustermann",30000,"Marketing"]
+    ]
 
-employees = sorted(employees)
+    employees = sorted(employees)
 
-connection = sqlite3.connect("employees.db")
-cursor = connection.cursor()
-cursor.execute("DROP TABLE IF EXISTS employees")
+    connection = sqlite3.connect("employees.db")
+    cursor = connection.cursor()
+    cursor.execute("DROP TABLE IF EXISTS employees")
 
-cursor.execute("CREATE TABLE IF NOT EXISTS employees (id INTEGER PRIMARY KEY AUTOINCREMENT, surname TEXT, lastname TEXT, salery INTEGER, department TEXT)")
-for i in range(len(employees)):
-    cursor.execute("INSERT INTO employees (surname,lastname,salery,department) values (?,?,?,?)",[employees[i][0],employees[i][1],employees[i][2],employees[i][3]])
+    cursor.execute("CREATE TABLE IF NOT EXISTS employees (id INTEGER PRIMARY KEY AUTOINCREMENT, surname TEXT, lastname TEXT, salery INTEGER, department TEXT)")
+    for i in range(len(employees)):
+        cursor.execute("INSERT INTO employees (surname,lastname,salery,department) values (?,?,?,?)",[employees[i][0],employees[i][1],employees[i][2],employees[i][3]])
 
-connection.commit()
-connection.close()
+    connection.commit()
+    connection.close()
+
+if __name__ == '__main__':
+    init_db()
